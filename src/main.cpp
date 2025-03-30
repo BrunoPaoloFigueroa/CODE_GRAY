a76672af2236a84cd6f7aecb8aa5bbdd2ad43194
BrunoPaoloFigueroa <bruno.figueroa@usil.pe>
2025-03-29 21:14:37 -0500

    CoDIgo GRAY 100% funcional

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+
+
+#define F_cpu 16000000Ul
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main (void){
+
+  DDRD = 0xFF;  
+  char a = 0x00;  
+  char b= 0x01 ;
+
+  
+  while(1){
+
+    PORTD = a^(a>>1);  
+    _delay_ms(2000);    
+    a+=0x01;       
+
+    
+  }
+
+
+
+return 0;
+
+}
+
+
